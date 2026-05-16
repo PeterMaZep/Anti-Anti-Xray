7a50247a207e0865e073c155529b4733c6401e63
PeterMaZep <petermazep@pwindows.qzz.io>
2026-05-16 15:10:14 +0800

    Added Keybinds and Language files

--- src/client/java/net/pwindows/anti_anti_xray/client/Anti_anti_xrayClient.java
+++ src/client/java/net/pwindows/anti_anti_xray/client/Anti_anti_xrayClient.java
@@ -1,10 +1,38 @@
 package net.pwindows.anti_anti_xray.client;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.pwindows.anti_anti_xray.Anti_anti_xray;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class Anti_anti_xrayClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
     }
+
+    KeyMapping.Category CATEGORY = KeyMapping.Category.register(
+            Identifier.fromNamespaceAndPath(Anti_anti_xray.MOD_ID, "anti_anti_xray")
+    );
+
+    KeyMapping OPEN_SETTINGS = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.antiantixray.opensettings",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_X,
+                    this.CATEGORY
+            )
+    );
+
+    KeyMapping PANIK_BUTTON = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.antiantixray.panikbutton",
+                    InputConstants.Type.KEYSYM,
+                    GLFW.GLFW_KEY_P,
+                    this.CATEGORY
+            )
+    );
 }
--- src/main/java/net/pwindows/anti_anti_xray/Anti_anti_xray.java
+++ src/main/java/net/pwindows/anti_anti_xray/Anti_anti_xray.java
@@ -3,6 +3,7 @@ package net.pwindows.anti_anti_xray;
 import net.fabricmc.api.ModInitializer;
 
 public class Anti_anti_xray implements ModInitializer {
+    public static final String MOD_ID = "anti_anti_xray";
 
     @Override
     public void onInitialize() {
